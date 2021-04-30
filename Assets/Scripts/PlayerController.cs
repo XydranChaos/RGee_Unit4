8e45f7479dc8b3be65338cf969a5d75eaf2c3ea7
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-04-29 22:19:59 -0700

    Finished Unit 4 Lesson A

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rbPlayer;
+    GameObject focalPoint;
+    Renderer rendererPlayer;
+    public float speed = 10.0f;
+    
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        rendererPlayer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float magnitude = forwardInput * speed * Time.deltaTime;
+        rbPlayer.AddForce(focalPoint.transform.forward * forwardInput * speed * Time.deltaTime, ForceMode.Impulse);
+
+        Debug.Log("Mag: " + magnitude);
+        Debug.Log("FI: " + forwardInput);
+
+        
+        if(forwardInput > 0)
+        {
+            rendererPlayer.material.color = new Color(1.0f - forwardInput, 1.0f, 1.0f - forwardInput);
+        }
+        else
+        {
+            rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotateInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, rotateInput * speed * Time.deltaTime);
+    }
+}
