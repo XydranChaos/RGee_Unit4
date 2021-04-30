1f3f6adc67042e8fc50d26c472abc2a94ad63ce5
XydranChaos <78708475+XydranChaos@users.noreply.github.com>
2021-04-29 23:15:17 -0700

    Finished Unit 4 Lesson B

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody enemyRB;
+    GameObject player;
+    public float speed = 4.0f;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        enemyRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //enemy will have a distance between the player when chasing.
+        Vector3 seekDirection = (player.transform.position - transform.position).normalized;
+        enemyRB.AddForce( seekDirection * speed * Time.deltaTime);
+    }
+}
